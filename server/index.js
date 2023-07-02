89a4bbc817c215ba9ab67acd0a74e2ee598df511
sm3llyCAT00 <pis.an6991@gmail.com>
2023-07-02 12:36:37 +0800

    Added JWT for admin and user verification, add user login and registration.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
@@ -24,7 +25,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
-
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,33 @@
 import express from "express";
+import {
+  deleteUser,
+  getAllUser,
+  getUser,
+  updateUser,
+} from "../controllers/userController.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+// router.get("/checkAuthentication", verifyToken, (req, res, next) => {
+//   res.send("Hello, you are logged in.");
+// });
+
+// router.get("/checkUser/:id", verifyUser, (req, res, next) => {
+//   res.send("Hello, you are logged in.");
+// });
+
+// router.get("/checkAdmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("Hello Admin, you are logged in.");
+// });
+
+// UPDATE
+router.put("/:id", verifyUser, updateUser);
+// DELETE
+router.delete("/:id", verifyUser, deleteUser);
+// GET
+router.get("/:id", verifyUser, getUser);
+// GET ALL
+router.get("/", verifyAdmin, getAllUser);
 
 export default router;
